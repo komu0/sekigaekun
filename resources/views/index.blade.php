ed1b16fd4978d7587c88094e9a906b5a70784331
komu <komu.engineer@gmail.com>
2020-10-06 05:54:48 +0000

    Vue導入。

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>席替えくん(チーム分け君)</title>
+
+  <!-- Scripts -->
+  <script src="{{ mix('js/app.js') }}" defer></script>
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+</head>
+<body>
+  <div id="app"></div>
+</body>
+</html>
